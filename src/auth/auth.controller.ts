77aaa2128c16d21c3d26a006b628d8d9a9aa706d
Matheus Geraldo <73797734+matheusgmc@users.noreply.github.com>
2024-01-26 20:40:22 -0400

    Feat/swagger (#6)
    * feat: add swagger
    
    * feat: add response body
    
    ---------
    
    Co-authored-by: Natan Gabriel Almeida de Castro <castro.natan@outlook.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,29 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignInDTO } from './dtos/sign-in.dto';
-import { SignUpDTO } from './dtos/sign-up.dto';
+import { SignInRequestDTO, SignInResponseDTO } from './dtos/sign-in.dto';
+import { SignUpRequestDTO, SignUpResponseDTO } from './dtos/sign-up.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('sign-in')
-  async signIn(@Body() body: SignInDTO) {
+  @ApiOkResponse({
+    isArray: false,
+    type: SignInResponseDTO,
+  })
+  async signIn(@Body() body: SignInRequestDTO) {
     return this.authService.signIn(body);
   }
 
   @Post('sign-up')
-  async signUp(@Body() body: SignUpDTO) {
+  @ApiCreatedResponse({
+    isArray: false,
+    type: SignUpResponseDTO,
+  })
+  async signUp(@Body() body: SignUpRequestDTO) {
     return this.authService.signUp(body);
   }
 }
--- src/auth/dtos/sign-in.dto.ts
+++ src/auth/dtos/sign-in.dto.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { AccountEntityToObject } from 'src/account/domain/account.entity';
 
-export class SignInDTO {
+export class SignInRequestDTO {
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
+}
+
+export class SignInResponseDTO {
+  @ApiProperty()
+  token: string;
+  @ApiProperty()
+  account: Omit<AccountEntityToObject, 'id'>;
 }
