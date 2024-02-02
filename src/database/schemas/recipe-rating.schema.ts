016d9f885d83640d1028574203decc9f99c16a06
Matheus Geraldo <matheus.geraldo1@outlook.com>
2024-02-02 18:55:54 -0400

    feat: add relation

--- src/database/schemas/recipe-rating.schema.ts
+++ src/database/schemas/recipe-rating.schema.ts
@@ -2,6 +2,7 @@ import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 import * as crypto from 'crypto';
 import { recipe_schema } from './recipe.schema';
 import { account_schema } from './account.schema';
+import { relations } from 'drizzle-orm';
 
 export const recipe_rating_schema = sqliteTable('recipe_rating', {
   id: text('id')
@@ -15,3 +16,17 @@ export const recipe_rating_schema = sqliteTable('recipe_rating', {
     .notNull()
     .references(() => account_schema.id),
 });
+
+export const recipe_rating_relations = relations(
+  recipe_rating_schema,
+  ({ one }) => ({
+    account: one(account_schema, {
+      fields: [recipe_rating_schema.id],
+      references: [account_schema.id],
+    }),
+    recipe: one(recipe_schema, {
+      fields: [recipe_rating_schema.id],
+      references: [recipe_schema.id],
+    }),
+  }),
+);
