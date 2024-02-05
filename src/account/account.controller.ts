3d891aba52e4554fdea4d8c26923fbf006f2877a
Natan Gabriel Almeida de castro <castro.natan@outlook.com>
2024-02-04 23:19:37 -0300

    Update account.controller.ts

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -15,17 +15,17 @@ import { Request } from 'express';
 
 @Controller('account')
 export class AccountController {
-  constructor(private readonly accountSerivce: AccountService) {}
+  constructor(private readonly accountService: AccountService) {}
 
   @Get(':id')
   findById(@Param('id', ParseUUIDPipe) id: string) {
-    return this.accountSerivce.findById(id);
+    return this.accountService.findById(id);
   }
 
   @UseGuards(AuthGuard)
   @Put()
   update(@Req() request: Request, @Body() body: UpdateAccountDTO) {
     const id = request.account.id;
-    this.accountSerivce.update(id, body);
+    this.accountService.update(id, body);
   }
 }
